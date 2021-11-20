2163461c7f25fe8c7b0446f5fbddfc0b027dc9fa
Garvit Saxena <garvitsaxena06@gmail.com>
2021-11-20 22:25:32 +0530

    Conversations and Messages API done

---
+++ api/routes/conversations.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+const Conversation = require('../models/Conversation')
+
+//new conversation
+
+router.post('/', async (req, res) => {
+  const { senderId, receiverId } = req.body
+  const newConversation = new Conversation({
+    members: [senderId, receiverId],
+  })
+
+  try {
+    const savedConversation = await newConversation.save()
+    res.status(201).json(savedConversation)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+//get conversation of a user
+
+router.get('/:userId', async (req, res) => {
+  try {
+    const conversation = await Conversation.find({
+      members: { $in: [req.params.userId] },
+    })
+    res.status(200).json(conversation)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+module.exports = router
