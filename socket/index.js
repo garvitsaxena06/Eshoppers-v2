e8679ec5688625bbe8073a89f279f45f9e7159de
Garvit Saxena <garvitsaxena06@gmail.com>
2021-12-02 19:26:51 +0530

    CORS headers changed

--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,6 @@
 const io = require('socket.io')(8900, {
   cors: {
-    origin: [
-      'http://localhost:5000',
-      'http://localhost:3000',
-      'http://13.232.184.56',
-      'https://13.232.184.56',
-    ],
+    origin: '*',
   },
 })
 
