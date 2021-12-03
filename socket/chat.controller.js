aa66d198ef91e5d009d933a760b7774dedf7c1f3
Jess70 <patidarjayesh27@gmail.com>
2021-12-03 19:50:45 +0530

    Socket config changed

---
+++ socket/chat.controller.js
@@ -0,0 +1,63 @@
+let users = []
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId })
+}
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId)
+}
+
+const removeUserByUserId = (userId) => {
+  users = users.filter((user) => user.userId !== userId)
+}
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId)
+}
+
+function ChatController(socket) {
+  // when connect
+  console.log('a user connected.')
+
+  // take userId and socketId from user
+  socket.on('addUser', (userId) => {
+    addUser(userId, socket.id)
+    io.emit('getUsers', users)
+  })
+
+  // send and get message
+  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId)
+    io.to(user?.socketId).emit('getMessage', {
+      senderId,
+      text,
+    })
+  })
+
+  // add conversation
+  socket.on('addConversation', ({ senderId, receiverId, conversation }) => {
+    const user = getUser(receiverId)
+    io.to(user?.socketId).emit('getConversation', {
+      senderId,
+      conversation,
+    })
+  })
+
+  // logout user
+  socket.on('logoutUser', (id) => {
+    console.log('a user disconnected.')
+    removeUserByUserId(id)
+    io.emit('getUsers', users)
+  })
+
+  // disconnect a user
+  socket.on('disconnect', () => {
+    console.log('a user disconnected.')
+    removeUser(socket.id)
+    io.emit('getUsers', users)
+  })
+}
+
+module.exports = { ChatController }
--- socket/index.js
+++ socket/index.js
@@ -1,3 +1,4 @@
+const { ChatController } = require('./chat.controller')
 const io = require('socket.io')(8900, {
   cors: {
     origin: [
@@ -11,64 +12,14 @@ const io = require('socket.io')(8900, {
   },
 })
 
-let users = []
+const ChatIO = io.of("/chat");
 
-const addUser = (userId, socketId) => {
-  !users.some((user) => user.userId === userId) &&
-    users.push({ userId, socketId })
-}
 
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId)
-}
+ChatIO.on("connection", ChatController);
 
-const removeUserByUserId = (userId) => {
-  users = users.filter((user) => user.userId !== userId)
-}
 
-const getUser = (userId) => {
-  return users.find((user) => user.userId === userId)
-}
+
 
 io.on('connection', (socket) => {
-  // when connect
-  console.log('a user connected.')
-
-  // take userId and socketId from user
-  socket.on('addUser', (userId) => {
-    addUser(userId, socket.id)
-    io.emit('getUsers', users)
-  })
-
-  // send and get message
-  socket.on('sendMessage', ({ senderId, receiverId, text }) => {
-    const user = getUser(receiverId)
-    io.to(user?.socketId).emit('getMessage', {
-      senderId,
-      text,
-    })
-  })
-
-  // add conversation
-  socket.on('addConversation', ({ senderId, receiverId, conversation }) => {
-    const user = getUser(receiverId)
-    io.to(user?.socketId).emit('getConversation', {
-      senderId,
-      conversation,
-    })
-  })
-
-  // logout user
-  socket.on('logoutUser', (id) => {
-    console.log('a user disconnected.')
-    removeUserByUserId(id)
-    io.emit('getUsers', users)
-  })
-
-  // disconnect a user
-  socket.on('disconnect', () => {
-    console.log('a user disconnected.')
-    removeUser(socket.id)
-    io.emit('getUsers', users)
-  })
+  
 })
