6ac5209947973cf94305f332edd29d0b786e7fff
Garvit Saxena <garvitsaxena06@gmail.com>
2022-11-20 16:51:28 +0530

    Changes uptil now

--- client/src/utils/connections.js
+++ client/src/utils/connections.js
@@ -1,7 +1,15 @@
-export const POSTS_BASE_URL = "https://k5gxjlwjl0.execute-api.ap-south-1.amazonaws.com/posts";
+export const BASE_URL = ''
 
-export const USERS_BASE_URL = "https://2r0sg4bxg5.execute-api.ap-south-1.amazonaws.com/users";
+export const POSTS_BASE_URL =
+  'https://k5gxjlwjl0.execute-api.ap-south-1.amazonaws.com/posts'
 
-export const CONVERSATIONS_BASE_URL = "https://95rv9rmrxi.execute-api.ap-south-1.amazonaws.com/conversations";
+export const USERS_BASE_URL =
+  'https://2r0sg4bxg5.execute-api.ap-south-1.amazonaws.com/users'
 
-export const MESSAGES_BASE_URL = "https://5kq2t3b9fi.execute-api.ap-south-1.amazonaws.com/messages";
+export const AUTH_BASE_URL = ''
+
+export const CONVERSATIONS_BASE_URL =
+  'https://95rv9rmrxi.execute-api.ap-south-1.amazonaws.com/conversations'
+
+export const MESSAGES_BASE_URL =
+  'https://5kq2t3b9fi.execute-api.ap-south-1.amazonaws.com/messages'
